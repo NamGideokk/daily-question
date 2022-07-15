62e26944aa392a5065261d780f994af8e447e511
Nam Gideok <namgd1222@gmail.com>
2022-07-15 21:26:46 +0900

    알고리즘 백준

---
+++ java/q034.java
@@ -0,0 +1,28 @@
+// 더하기 사이클 (백준 1110번)
+import java.util.Scanner;
+
+public class q034 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int copy = a;
+        int count = 0;
+
+        while(a < 0 | a > 99){
+            System.out.print("0 <= a >= 99 : ");
+            a = sc.nextInt();
+        }
+
+        while(true){
+            a = ((a % 10) * 10) + (((a / 10) + (a % 10)) % 10);
+            count++;
+            
+            if(copy == a){
+                break;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
