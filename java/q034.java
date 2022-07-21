36dfd8174fed177df9ef5f888b2b562a984dc623
Nam Gideok <namgd1222@gmail.com>
2022-07-21 21:25:19 +0900

    알고리즘 (백준 1110번) q034 수정

--- java/q034.java
+++ java/q034.java
@@ -9,19 +9,21 @@ public class q034 {
         int copy = a;
         int count = 0;
 
+        
+
         while(a < 0 | a > 99){
             System.out.print("0 <= a >= 99 : ");
             a = sc.nextInt();
         }
 
-        while(true){
+        do{
             a = ((a % 10) * 10) + (((a / 10) + (a % 10)) % 10);
             count++;
             
             if(copy == a){
                 break;
             }
-        }
+        }while(copy != a);
 
         System.out.println(count);
     }
