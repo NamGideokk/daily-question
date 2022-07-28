e2eafadf197ee3156c78a176e666c88afff3d83a
Nam Gideok <namgd1222@gmail.com>
2022-07-28 23:20:31 +0900

    알고리즘 프로그래머스

--- java-script/q020.js
+++ java-script/q020.js
@@ -2,17 +2,25 @@
 // 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성되어 있으면 true.
 
 function solution(s) {
-  let answer = true;
-
   // 먼저 길이가 4 or 6이 아닐경우 early exit
-  if (s.length != 4 && s.length != 6) {
+  if ((s.length != 4 && s.length != 6) || s.length > 8) {
     return false;
   }
 
-  let arr = s.split("");
-  let result = arr.every((val) => typeof Number(val) == "number");
+  // 자바스크립트는 문자가 섞여있어도 Number()로 형변환을 하면 type은 Number로 나온다.
+  // let typeCheck = Number(s);
 
-  return result;
+  // 그래서 형변환된 변수에 + 1 더해 값이 제대로 있으면 true
+  // 제대로 문자가 포함되어 NaN이 찍히면 false를 이용하여 answer 반환.
+  // if (typeCheck + 1) {
+  //   return answer;
+  // } else {
+  //   answer = false;
+  //   return answer;
+  // }
+
+  let arr = s.split("");
+  return arr.every((val) => !isNaN(val));
 }
 
 console.log(solution("aBCD")); // false
@@ -23,3 +31,4 @@ console.log(solution("bb34")); // false
 console.log(solution("134")); // false
 console.log(solution("123456")); // true
 console.log(solution("123b56")); // false
+console.log(solution("10e1")); // false
