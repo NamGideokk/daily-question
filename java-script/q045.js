103d28f5490bd3497decce83707049261dad17a2
Nam Gideok <namgd1222@gmail.com>
2022-09-02 22:52:42 +0900

    알고리즘 프로그래머스

---
+++ java-script/q045.js
@@ -0,0 +1,12 @@
+// 내적 (프로그래머스)
+function solution(a, b) {
+  var answer = 0;
+  for (let i = 0; i < a.length; i++) {
+    answer += a[i] * b[i];
+  }
+
+  return answer;
+}
+
+console.log(solution([1, 2, 3, 4], [-3, -1, 0, 2]));
+console.log(solution([-1, 0, 1], [1, 0, -1]));
